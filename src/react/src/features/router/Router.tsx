112964d2ad97a4717e3c6a4ca9f8343aa842b42f
dorisgjata <dorisgjata@oakland.edu>
2021-10-30 17:10:43 -0400

    feat:  updating path on tab selection

--- src/react/src/features/router/Router.tsx
+++ src/react/src/features/router/Router.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Box, Button, Grid, Typography, Tabs, Tab } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { handleSelect } from './tabsSlice';
+import { handleSelect, indexToTab as tabs } from './tabsSlice';
 import { Collection } from '../collection/Collection';
 import { getCollection } from '../collection/collectionSlice';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
 export function Router() {
-    const tabValue = useAppSelector((state) => state.tabs.tab);
+    const { tab: tabValue, path } = useAppSelector((state) => state.tabs);
+    let tabComponent = [];
+    for (const [path, index] of Object.entries(tabs)) {
+        tabComponent.push(<Tab key={`${index}_${path}`} label={path}
+            to={`/${path}`} value={index} component={Link} />)
+    }
     const collectionStatus = useAppSelector(state => state.collection.status);
     const dispatch = useAppDispatch();
     const handleTabs = (event: React.SyntheticEvent<Element, Event>, val: number) => {
@@ -17,15 +22,13 @@ export function Router() {
             <Grid container >
                 <Grid item xs={12}>
                     <Tabs value={tabValue} onChange={handleTabs} centered>
-                        <Tab label='Collection'
-                            to='/collection' component={Link} />
-                        <Tab label='Tour'
-                            to='/tour' component={Link} />
-                        <Tab label='Favorites'
-                            to='/favorites' component={Link} />
+                        {tabComponent}
                     </Tabs>
                     <Switch>
-                        <Route path="/collection">
+                        <Route exact path="/">
+                            <Redirect to="/collection" />
+                        </Route>
+                        <Route exact path="/collection">
                             <Grid item>
                                 <Typography style={{ padding: 10 }}>
                                     Carnegie Museum of Art is arguably the first museum of contemporary art in the United States, collecting the “Old Masters of tomorrow” since the inception of the Carnegie International in 1896.
@@ -37,9 +40,9 @@ export function Router() {
                                 {collectionStatus === 'idle' ? <Collection /> : ''}
                             </Grid>
                         </Route>
-                        <Route path="/tour">
+                        <Route exact path="/tour">
                         </Route>
-                        <Route path="/favorites">
+                        <Route exact path="/favorites">
                         </Route>
                     </Switch>
 
--- src/react/src/features/router/tabsSlice.ts
+++ src/react/src/features/router/tabsSlice.ts
@@ -1,13 +1,28 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface TabState {
-    tab: number
+    tab: number,
+    path: string,
 };
+
+
+export const tabToIndex: { [key: number]: string } = {
+    0: 'collection',
+    1: 'tour',
+    2: 'favorites'
+}
+export const indexToTab: { [key: string]: number } = {
+    collection: 0,
+    tour: 1,
+    favorites: 2
+}
 /* For some reason the state variable should be named 'initialState' 
  * and you can't name it 'initialTabState' or TS yells at you
 */
+
 const initialState: TabState = {
-    tab: 0
+    tab: 0,
+    path: tabToIndex[0]
 };
 
 export const tabsSlice = createSlice({
@@ -17,6 +32,7 @@ export const tabsSlice = createSlice({
         handleSelect: (state, action: PayloadAction<number>) => {
             console.log(action.payload);
             state.tab = action.payload;
+            state.path = tabToIndex[action.payload];
         }
     }
 });
