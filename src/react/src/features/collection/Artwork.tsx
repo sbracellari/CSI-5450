1a974458ac1451dccc48def340bc7671beaf762e
dorisgjata <dorisgjata@oakland.edu>
2021-10-30 18:57:15 -0400

    feat: changing collection to use artwok

--- src/react/src/features/collection/Artwork.tsx
+++ src/react/src/features/collection/Artwork.tsx
@@ -1,17 +1,43 @@
-import { Card, CardContent, Typography } from "@mui/material";
 import { Artwork as ArtworkType } from "./collectionSlice";
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Grid, IconButton, Typography } from "@mui/material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Link } from 'react-router-dom';
 
-const Artwork = (artwork: ArtworkType) => {
+interface ArtworkProps {
+    artwork: ArtworkType;
+}
+
+export function Artwork(props: ArtworkProps) {
+    const { artwork } = props;
     return (
-        <div>
-            <Card>
-                <CardContent>
-                    <Typography>
+        <Card variant="outlined" sx={{ pb: 2 }}>
+            <CardMedia
+                component="img"
+                sx={{ width: 200 }}
+                image="../../../public/logo512.png"
+            />
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent >
+                    <Typography component="div" variant="h6">
                         {artwork.title}
                     </Typography>
+                    <Typography variant="subtitle1" color="text.secondary" component="div">
+                        {artwork.creator.fullName} - {artwork.creationDate}
+                    </Typography>
                 </CardContent>
-            </Card>
-        </div>
+                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1, gap: 2 }}>
+                    <Chip color="info" label={artwork.medium} />
+                    <Chip color="secondary" label={artwork.classification} />
+                </Box>
+            </Box>
+            <CardActions>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <Button size="small" aria-label="view details" component={Link} to={`/details?id=${artwork.id}`}>Details</Button>
+                <Button size="small" aria-label="add to tour" >Add to tour</Button>
+            </CardActions>
+        </Card>
+
     )
 };
-export default Artwork;
--- src/react/src/features/collection/Collection.tsx
+++ src/react/src/features/collection/Collection.tsx
@@ -1,8 +1,7 @@
 
 import { useAppSelector } from '../../app/hooks';
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Grid, IconButton, Typography } from "@mui/material";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Link } from 'react-router-dom';
+import { Grid } from "@mui/material";
+import { Artwork } from './Artwork';
 
 export function Collection() {
   const collection = useAppSelector((state) => state.collection.collection);
@@ -12,34 +11,7 @@ export function Collection() {
       {collection.map((artwork, index) => {
         return (
           <Grid item xs={11}>
-            <Card key={index} variant="outlined" sx={{ pb: 2 }}>
-              <CardMedia
-                component="img"
-                sx={{ width: 200 }}
-                image="../../../public/logo512.png"
-              />
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent >
-                  <Typography component="div" variant="h6">
-                    {artwork.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="text.secondary" component="div">
-                    {artwork.creator.fullName} - {artwork.creationDate}
-                  </Typography>
-                </CardContent>
-                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1, gap: 2 }}>
-                  <Chip color="info" label={artwork.medium} />
-                  <Chip color="secondary" label={artwork.classification} />
-                </Box>
-              </Box>
-              <CardActions>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <Button size="small" aria-label="view details" component={Link} to={`/details?id=${artwork.id}`}>Details</Button>
-                <Button size="small" aria-label="add to tour" >Add to tour</Button>
-              </CardActions>
-            </Card>
+            <Artwork artwork={artwork}/>
           </Grid>
         )
       })}
