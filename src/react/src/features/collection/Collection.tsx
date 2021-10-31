1ae6d7dca8b06884dbf429f93331b4fcbe355918
dorisgjata <dorisgjata@oakland.edu>
2021-10-30 20:13:59 -0400

    feat: reorganizing routes

--- src/react/src/features/collection/Collection.tsx
+++ src/react/src/features/collection/Collection.tsx
@@ -1,17 +1,28 @@
 
-import { useAppSelector } from '../../app/hooks';
-import { Grid } from "@mui/material";
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { Artwork } from './Artwork';
+import { getCollection } from './collectionSlice';
 
 export function Collection() {
-  const collection = useAppSelector((state) => state.collection.collection);
+  const { collection, status } = useAppSelector((state) => state.collection);
+  const dispatch = useAppDispatch();
 
   return (
-    <Grid container spacing={2} justifyContent="center" >
+    <Grid container spacing={1} justifyContent="center" >
+      <Grid item xs={12}>
+        <Typography sx={{ p: 2 }}>
+          Carnegie Museum of Art is arguably the first museum of contemporary art in the United States, collecting the “Old Masters of tomorrow” since the inception of the Carnegie International in 1896.
+          Our collection of more than 34,000 objects features a broad spectrum of visual art, including painting and sculpture; prints and drawings; photographs; architectural casts, renderings, and models; decorative arts and design; and film, video, and digital imagery. The museum also houses the archive of over 70,000 negatives by photographer Charles “Teenie” Harris.
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', pb: 2 }}>
+          <Button onClick={() => dispatch(getCollection())} variant="contained">View collection</Button>
+        </Box>
+      </Grid>
       {collection.map((artwork, index) => {
         return (
-          <Grid item xs={11}>
-            <Artwork artwork={artwork}/>
+          <Grid key={`${index}_${artwork.title}`} item xs={11}>
+            <Artwork artwork={artwork} />
           </Grid>
         )
       })}
--- src/react/src/features/router/Router.tsx
+++ src/react/src/features/router/Router.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Box, Button, Grid, Typography, Tabs, Tab } from '@mui/material';
+import { Tabs, Tab } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { handleSelect, indexToTab as tabs } from './tabsSlice';
 import { Collection } from '../collection/Collection';
-import { getCollection } from '../collection/collectionSlice';
 import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
+import { Detail } from '../collection/Detail';
 export function Router() {
     const { tab: tabValue, path } = useAppSelector((state) => state.tabs);
     let tabComponent = [];
@@ -12,42 +12,24 @@ export function Router() {
         tabComponent.push(<Tab key={`${index}_${path}`} label={path}
             to={`/${path}`} value={index} component={Link} />)
     }
-    const collectionStatus = useAppSelector(state => state.collection.status);
     const dispatch = useAppDispatch();
     const handleTabs = (event: React.SyntheticEvent<Element, Event>, val: number) => {
         dispatch(handleSelect(val));
     };
     return (
-         <BrowserRouter basename='/'>
-            <Grid container >
-                <Grid item xs={12}>
-                    <Tabs value={tabValue} onChange={handleTabs} centered>
-                        {tabComponent}
-                    </Tabs>
-                    <Switch>
-                        <Route exact path="/">
-                            <Redirect to="/collection" />
-                        </Route>
-                        <Route exact path="/collection">
-                            <Grid item>
-                                <Typography sx={{ p: 2 }}>
-                                    Carnegie Museum of Art is arguably the first museum of contemporary art in the United States, collecting the “Old Masters of tomorrow” since the inception of the Carnegie International in 1896.
-                                    Our collection of more than 34,000 objects features a broad spectrum of visual art, including painting and sculpture; prints and drawings; photographs; architectural casts, renderings, and models; decorative arts and design; and film, video, and digital imagery. The museum also houses the archive of over 70,000 negatives by photographer Charles “Teenie” Harris.
-                                </Typography>
-                                <Box sx={{ display: 'flex', justifyContent: 'center', pb: 2 }}>
-                                    <Button onClick={() => dispatch(getCollection())} variant="contained">View collection</Button>
-                                </Box>
-                                {collectionStatus === 'idle' ? <Collection /> : ''}
-                            </Grid>
-                        </Route>
-                        <Route exact path="/tour">
-                        </Route>
-                        <Route exact path="/favorites">
-                        </Route>
-                    </Switch>
-
-                </Grid>
-            </Grid>
+        <BrowserRouter basename='/'>
+            <Tabs value={tabValue} onChange={handleTabs} centered>
+                {tabComponent}
+            </Tabs>
+            <Switch>
+                <Route exact path="/">
+                    <Redirect to="/collection" />
+                </Route>
+                <Route exact path="/collection" component={Collection} />
+                <Route exact path="/tour" />
+                <Route exact path="/favorites" />
+                <Route path='/details' component={Detail} />
+            </Switch>
         </BrowserRouter>
     )
 }
