b5ef47440c28f184659e84d040c4044f95e4d354
dorisgjata <dorisgjata@oakland.edu>
2021-11-04 22:29:02 -0400

    feat: add rowmapper to grab db data fro artwork

--- src/main/java/edu/oakland/model/Artwork.java
+++ src/main/java/edu/oakland/model/Artwork.java
@@ -19,5 +19,39 @@ public class Artwork {
     private String imageUrl;
     private String classification;
     private Creator creator;
-    //@todo: add rowmapper to grab db data
+    private Location location;
+
+    public static RowMapper<Artwork> mapper =
+        (rs, rowNum) -> {
+            Artwork artwork = new Artwork();
+            artwork.setId(rs.getString("id"));
+            artwork.setTitle(rs.getString("title"));
+            artwork.setCreationDate(rs.getString("creation_date"));
+            artwork.setMedium(rs.getString("medium"));
+            artwork.setCreditLine(rs.getString("credit_line"));
+            artwork.setDateAquired(rs.getString("date_acquired"));
+            artwork.provenanceText(rs.getString("provenance_text"));
+            artwork.setProvenanceText(rs.getString("classification"));
+            artwork.setCreator(
+                new Creator(
+                    rs.getString('artist_id'),
+                    rs.getString('full_name'),
+                    rs.getString('cited_name'),
+                    rs.getString('role'),
+                    rs.getString('nationality'),
+                    rs.getString('birth_date'),
+                    rs.getString('death_date'),
+                    rs.getString('birth_place'),
+                    rs.getString('death_place')
+                )
+            );
+            artwork.setLocation(
+                new Location(
+                    rs.getString('id'),
+                    rs.getString('departament'),
+                    rs.getString('physical_location')
+                )
+            );
+        return artwork;
+    };
 }
