7425d8e45a2be00f3d5168af93a834b1fac7cbc9
dorisgjata <dorisgjata@oakland.edu>
2021-11-07 17:11:01 -0500

    feat: adding initial controller

---
+++ src/main/java/edu/oakland/arttour/controller/ArtTourController.java
@@ -0,0 +1,80 @@
+package edu.oakland.arttour.controller;
+
+import edu.oakland.arttour.model.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+// import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class ArtTourController {
+
+  private final Logger log = LoggerFactory.getLogger("arttour");
+
+//   @Autowired private ArtTourDao dao;
+
+//   @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Illegal Arguments given")
+//   @ExceptionHandler({IllegalArgumentException.class, DataAccessException.class})
+//   public void illegalArgumentError(Exception e) {
+    // log.error("Throwing Illegal Argument or Data Access error", e);
+//   }
+
+  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Unspecified exception")
+  @ExceptionHandler(Exception.class)
+  public void generalError(Exception e) {
+    log.error("Unspecified exception", e);
+  }
+
+  @GetMapping("collection")
+  public List<Artwork> getCollection() {
+        List collectionsList = new ArrayList<Artwork>();
+        Creator creator = new Creator(
+                                    "artistId",
+                                    "fullName",
+                                    "citedName",
+                                    "role",
+                                    "nationality",
+                                    "birthDate",
+                                    "deathDate",
+                                    "birthPlace",
+                                    "deathPlace"
+                                    );
+        Location location = new Location(
+                                    "locationId",
+                                    "departament",
+                                    "physicalLocation"
+                                    );
+        Artwork collection1 = new Artwork(
+                                    "id",
+                                    "title",
+                                    "creationDate",
+                                    "medium",
+                                    "creditLine",
+                                    "dateAquired",
+                                    "provenanceText",
+                                    "imageUrl",
+                                    "classification",
+                                    creator,
+                                    location
+                                    );
+        collectionsList.add(collection1);
+        return collectionsList;
+
+  }
+}
