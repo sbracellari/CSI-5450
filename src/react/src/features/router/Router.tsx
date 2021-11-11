807b0609870be216a3a4038eb42955b977db15bb
dorisgjata <dorisgjata@oakland.edu>
2021-11-11 14:32:25 -0500

    feat: add tour stepper

--- src/react/src/features/router/Router.tsx
+++ src/react/src/features/router/Router.tsx
@@ -8,6 +8,7 @@ import { Detail } from '../collection/Detail';
 import { Login } from '../auth/Login';
 import { Register } from '../auth/Register';
 import { Tours } from '../tour/Tours';
+import { TourStepper } from '../tour/TourStepper';
 
 export function Router() {
     const { tab: tabValue, path } = useAppSelector((state) => state.tabs);
@@ -32,6 +33,7 @@ export function Router() {
                 </Route>
                 <Route exact path="/collection" component={Collection} />
                 <Route exact path="/tour" component={Tours} />
+                <Route path="/tour:tourId" component={TourStepper} />
                 <Route exact path="/favorites" />
                 <Route path='/details' component={Detail} />
                 <Route path='/account/login' component={Login} />
