1f36b806c1e42fb890bb8d79b78644c9f58ca403
dorisgjata <dorisgjata@oakland.edu>
2021-11-12 18:03:40 -0500

    feat: had to add corssorigin for the apis to work

--- src/main/java/edu/oakland/arttour/controller/ArtTourController.java
+++ src/main/java/edu/oakland/arttour/controller/ArtTourController.java
@@ -57,7 +57,7 @@ public class ArtTourController {
   public List<Artwork> getCollection() {
     return dao.getCollection();
   }
-
+  @CrossOrigin
   @GetMapping("tours")
   public List<Tour> getPublicTours() {
     return service.getPublicTours();
@@ -66,7 +66,7 @@ public class ArtTourController {
   // @TODO filtered collection
   // @GetMapping("filtered-collection")
   // public List<Artwork> getFilteredCollection() {}
-
+  @CrossOrigin
   @GetMapping("locations")
   public List<Location> getAllLocations() {
     return dao.getAllLocations();
@@ -75,48 +75,56 @@ public class ArtTourController {
   /////////// general insert/update/delete //////////
 
   ///// inserts /////
+  @CrossOrigin
   @PostMapping("artwork/creator/addition")
   public void addArtwork(@RequestBody Map<Object, Object> artwork) {
     service.addArtwork(artwork);
   }
-
+  @CrossOrigin
   @PostMapping("location/addition")
   public void addLocation(@RequestBody Map<String, String> location) {
     service.addLocation(location);
   }
 
   ///// updates /////
+  @CrossOrigin
   @PostMapping("artwork/update")
   public void updateArtwork(@RequestBody Map<Object, Object> artwork) {
     service.updateArtwork(artwork);
   }
 
+  @CrossOrigin
   @PostMapping("creator/update")
   public void updateCreator(@RequestBody Map<Object, Object> creator) {
     service.updateCreator(creator);
   }
 
+  @CrossOrigin
   @PostMapping("location/update")
   public void updateLocation(@RequestBody Map<Object, Object> location) {
     service.updateLocation(location);
   }
 
   ///// deletes /////
+  @CrossOrigin
   @PostMapping("artwork/{artworkId}/removal")
   public void deleteArtwork(@PathVariable String artworkId) {
     dao.deleteArtwork(artworkId);
   }
 
+  @CrossOrigin
   @PostMapping("creator/{creatorId}/removal")
   public void deleteCreator(@PathVariable int creatorId) {
     dao.deleteCreator(creatorId);
   }
 
+  @CrossOrigin
   @PostMapping("location/{locationId}/removal")
   public void deleteLocation(@PathVariable int locationId) {
     dao.deleteLocation(locationId);
   }
 
+  @CrossOrigin
   @PostMapping("tour/{tourId}/removal")
   public void deleteTour(@PathVariable int tourId) {
     dao.deleteTour(tourId);
@@ -125,89 +133,106 @@ public class ArtTourController {
   ////////// consumer specific actions //////////
 
   ///// favorites /////
+  @CrossOrigin
   @GetMapping("consumer/{email}/favorites")
   public Favorite getUserFavorites(@PathVariable String email) {
     return service.getFavoritesForUser(email);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/artwork/{artworkId}")
   public void favoriteArtwork(@PathVariable String email, @PathVariable String artworkId) {
     dao.favoriteArtwork(email, artworkId);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/creator/{creatorId}")
   public void favoriteCreator(@PathVariable String email, @PathVariable int creatorId) {
     dao.favoriteCreator(email, creatorId);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/tour/{tourId}")
   public void favoriteTour(@PathVariable String email, @PathVariable int tourId) {
     dao.favoriteTour(email, tourId);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/artwork/{artworkId}/removal")
   public void deleteFavoriteArtwork(@PathVariable String email, @PathVariable String artworkId) {
     dao.deleteFavoriteArtwork(email, artworkId);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/creator/{creatorId}/removal")
   public void deleteFavoriteCreator(@PathVariable String email, @PathVariable int creatorId) {
     dao.deleteFavoriteCreator(email, creatorId);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/favorites/tour/{tourId}/removal")
   public void deleteFavoriteTour(@PathVariable String email, @PathVariable int tourId) {
     dao.deleteFavoriteTour(email, tourId);
   }
 
   ///// tours /////
+  @CrossOrigin
   @GetMapping("consumer/{email}/tours")
   public List<Tour> getToursForUser(@PathVariable String email) {
     return service.getToursForUser(email);
   }
 
+  @CrossOrigin
   @PostMapping("consumer/{email}/tour/creation")
   public void createTour(@PathVariable String email, @RequestBody String tourName) {
     dao.createTour(tourName, email);
   }
 
+  @CrossOrigin
   @PostMapping("tour/{tourId}/artwork/{artworkId}/add")
   public void addToTour(@PathVariable int tourId, @PathVariable String artworkId) {
     dao.addToTour(tourId, artworkId);
   }
 
+  @CrossOrigin
   @PostMapping("tour/{tourId}/artwork/{artworkId}/removal")
   public void deleteFromTour(@PathVariable int tourId, @PathVariable String artworkId) {
     dao.deleteFromTour(tourId, artworkId);
   }
 
+  @CrossOrigin
   @PostMapping("tour/{tourId}/update")
   public void updateTour(@RequestBody String tourName, @PathVariable int tourId) {
     dao.updateTour(tourName, tourId);
   }
 
   ////////// admin //////////
+  @CrossOrigin
   @GetMapping("admin/{email}")
   public Integer isAdmin(@PathVariable String email) {
     return dao.isAdmin(email);
   }
 
   ////////// general user //////////
+  @CrossOrigin
   @PostMapping("user/register")
   public boolean registerUser(@RequestBody Map<String, String> user) {
     return service.registerUser(user);
   }
 
+  @CrossOrigin
   @GetMapping("user/{email}/login")
   public Integer login(@RequestBody Map<String, String> userCreds) {
     return service.login(userCreds);
   }
 
+  @CrossOrigin
   @PostMapping("user/{email}/removal")
   public void deleteUser(@PathVariable String email) {
     dao.deleteUser(email);
   }
 
+  @CrossOrigin
   @PostMapping("user/update")
   public void updateUser(@RequestBody Map<String, String> user) {
     service.updateUser(user);
