ca0fdda6c2b4e99b41f4115708a49aeb6dc7752a
dorisgjata <dorisgjata@oakland.edu>
2021-11-11 20:10:50 -0500

    feat: tour data

---
+++ src/react/src/services/tourApi.ts
@@ -0,0 +1,416 @@
+import { Tour } from "../app/types";
+
+export const data: Tour[] =
+    [
+        {
+            "email": "admin1@arttour.com",
+            "tourName": "Admin 1 First Tour",
+            "tourId": 1,
+            "artworks": [
+                {
+                    "artworkId": "692a68c5-af1e-4124-80f1-cbf38be51abe",
+                    "title": "Keith Haring",
+                    "creationDate": "1984",
+                    "medium": "gelatin silver print",
+                    "creditLine": "Milton Fine Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 23.25,
+                    "itemHeight": 29.25,
+                    "itemDepth": 1.25,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "photographs",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 1,
+                        "fullName": "Robert Mapplethorpe",
+                        "citedName": "Mapplethorpe, Robert",
+                        "role": null,
+                        "nationality": "American",
+                        "birthDate": "1947-01-01",
+                        "deathDate": "1989-01-01",
+                        "birthPlace": "New York City (New York state, United States)",
+                        "deathPlace": "New York City (New York state, United States)"
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "96622b88-ed5f-44cf-8493-852c4b53fbad",
+                    "title": "Trans East West (Tew) No. 8: Suddenly It was Completely Dark (Maybe Because I Closed My Eyes)",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "9ee75ef7-d8a6-47bc-a0fe-dda1ec7d2984",
+                    "title": "Trans East West (Tew) No. 6: Rebuilding Beyrut",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.438,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "d4e7057c-6f1f-4bce-be3e-579fe5ab2af6",
+                    "title": "Trans East West (Tew) No. 7: Bombing the Power Plant",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "fe93f7e2-e4ca-455e-bdfa-ea9be22cce8a",
+                    "title": "Trans East West (Tew) No. 5: The Attack of the Embassy (Reconstruction)",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                }
+            ]
+        },
+        {
+            "email": "admin2@arttour.com",
+            "tourName": "Admin 1 Second Tour",
+            "tourId": 2,
+            "artworks": [
+                {
+                    "artworkId": "692a68c5-af1e-4124-80f1-cbf38be51abe",
+                    "title": "Keith Haring",
+                    "creationDate": "1984",
+                    "medium": "gelatin silver print",
+                    "creditLine": "Milton Fine Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 23.25,
+                    "itemHeight": 29.25,
+                    "itemDepth": 1.25,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "photographs",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 1,
+                        "fullName": "Robert Mapplethorpe",
+                        "citedName": "Mapplethorpe, Robert",
+                        "role": null,
+                        "nationality": "American",
+                        "birthDate": "1947-01-01",
+                        "deathDate": "1989-01-01",
+                        "birthPlace": "New York City (New York state, United States)",
+                        "deathPlace": "New York City (New York state, United States)"
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "96622b88-ed5f-44cf-8493-852c4b53fbad",
+                    "title": "Trans East West (Tew) No. 8: Suddenly It was Completely Dark (Maybe Because I Closed My Eyes)",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "9ee75ef7-d8a6-47bc-a0fe-dda1ec7d2984",
+                    "title": "Trans East West (Tew) No. 6: Rebuilding Beyrut",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.438,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "d4e7057c-6f1f-4bce-be3e-579fe5ab2af6",
+                    "title": "Trans East West (Tew) No. 7: Bombing the Power Plant",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "fe93f7e2-e4ca-455e-bdfa-ea9be22cce8a",
+                    "title": "Trans East West (Tew) No. 5: The Attack of the Embassy (Reconstruction)",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.5,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                }
+            ]
+        },
+        {
+            "email": "admin3@arttour.com",
+            "tourName": "Admin 3 First Tour",
+            "tourId": 3,
+            "artworks": [
+                {
+                    "artworkId": "08531bda-78ed-42c4-846d-dea2aade1896",
+                    "title": "Trans East West (Tew) No. 10: To the People of Damascus",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 12.875,
+                    "itemHeight": 10.563,
+                    "itemDepth": 1.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "41ee79b1-22cd-43c1-b112-31c63ba5cc68",
+                    "title": "Trans East West (Tew) No. 9: The Meridien Will Have a Nice View",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 12.875,
+                    "itemHeight": 10.563,
+                    "itemDepth": 1.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                },
+                {
+                    "artworkId": "9ee75ef7-d8a6-47bc-a0fe-dda1ec7d2984",
+                    "title": "Trans East West (Tew) No. 6: Rebuilding Beyrut",
+                    "creationDate": "1999",
+                    "medium": "mixed media on paper",
+                    "creditLine": "Second Century Acquisition Fund",
+                    "dateAcquired": "2002-06-06",
+                    "itemWidth": 7.438,
+                    "itemHeight": 5.125,
+                    "itemDepth": 0.0,
+                    "itemDiameter": 0.0,
+                    "provenanceText": "drawings and watercolors",
+                    "classification": null,
+                    "creator": {
+                        "creatorId": 3,
+                        "fullName": "Franz Ackermann",
+                        "citedName": "Ackermann, Franz",
+                        "role": null,
+                        "nationality": "German",
+                        "birthDate": "1963-01-01",
+                        "deathDate": null,
+                        "birthPlace": "Neumarkt-Sankt Veit (Oberbayern, Bavaria, Germany)",
+                        "deathPlace": null
+                    },
+                    "location": {
+                        "locationId": 1,
+                        "department": "Contemporary Art",
+                        "physicalLocation": "Not on View"
+                    }
+                }
+            ]
+        }
+    ];
